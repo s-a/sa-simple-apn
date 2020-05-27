f7e32c9fb86535214441c0135f4fd02c59d6cc25
s-a <stephan.ahlf@gmail.com>
2020-05-27 08:57:56 +0200

    fix eslint

--- test/apple-push-notification.spec.js
+++ test/apple-push-notification.spec.js
@@ -3,7 +3,6 @@ const ApnProvider = require('../lib/index.js')
 
 describe('Apple Push Notication', function () {
 	it('should at least build an instance', async function () {
-
 		const config = {
 			privatekey: null,
 			certificate: null
